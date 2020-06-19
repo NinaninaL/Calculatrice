27adb50d099ee87371e3600846edd094fd67be90
Nina <55928592+NinaninaL@users.noreply.github.com>
2020-06-19 15:11:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+function addition(nombreA, nombreB) {
+    return nombreA + nombreB;
+}
+
+function multiplication(nombreA, nombreB) {
+    return nombreA * nombreB;
+}
+
+function soustraction(nombreA, nombreB) {
+    return nombreA - nombreB;
+}
+
+function division(nombreA, nombreB) {
+    if (nombreB == 0) {
+        throw new Error("Il est interdit de diviser par 0 !");
+    }
+    return nombreA / nombreB;
+}
+
+let restart = false;
+do {
+    do {
+        var choix = Number(prompt("Que souhaitez-vous faire ? \n\n 1 ~ Addition \n 2 ~ Multiplication \n 3 ~ Soustraction \n 4 ~ Division"));
+    } while (choix != 1 && choix != 2 && choix != 3 && choix != 4); 
+
+
+    do {
+        var premierNombre  = Number(prompt("Entrer un premier nombre ?"));
+        var deuxiemeNombre  = Number(prompt("Entrer un deuxieme nombre ?"));
+    } while(isNaN(premierNombre) || isNaN(deuxiemeNombre));  
+
+    try {
+    switch(choix) {
+        case 1:
+            var resultat = addition(premierNombre, deuxiemeNombre);
+            break;
+
+        case 2:
+            var resultat = multiplication(premierNombre, deuxiemeNombre);
+            break;
+
+        case 3:
+            var resultat = soustraction(premierNombre, deuxiemeNombre);
+            break;
+
+        case 4:
+            var resultat = division(premierNombre, deuxiemeNombre);
+            break;
+        
+        default:
+            throw new Error("Une erreur est survenue !");
+    }
+        alert("Voici le resultat : " + resultat);
+    }
+    catch(error){
+        alert(error);
+    }
+
+        restart = confirm("Souhaitez-vous refaire un calcul ?");
+
+}while(restart);
